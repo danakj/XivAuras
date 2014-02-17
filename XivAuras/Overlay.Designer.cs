e4ffb74581e8a439e3ad1db5d7095f2ecac6187b
Dana Jansens <danakj@orodu.net>
2014-02-17 16:08:36 -0500

    Initial commit for XivAuras

---
+++ XivAuras/Overlay.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WindowsFormsApplication1 {
+    partial class Overlay {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1_ = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1_
+            // 
+            this.label1_.AutoSize = true;
+            this.label1_.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1_.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1_.Location = new System.Drawing.Point(41, 48);
+            this.label1_.Name = "label1_";
+            this.label1_.Size = new System.Drawing.Size(89, 26);
+            this.label1_.TabIndex = 0;
+            this.label1_.Text = "HELLO";
+            // 
+            // Overlay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1_);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Overlay";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "XivAuras";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1_;
+    }
+}
+
